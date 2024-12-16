3f7a1fcd6705c98363571c769899abbedd7be1f8
vijaykumarnavale <navalevijay85@gmail.com>
2024-12-17 00:11:59 +0530

    sql data tables v2

--- master.sql
+++ master.sql
@@ -1,34 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 21, 2024 at 03:23 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 7.4.33
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `master`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `full_name` varchar(100) DEFAULT NULL,
   `email` varchar(100) NOT NULL,
   `contact_number` varchar(20) DEFAULT NULL,
@@ -37,37 +9,9 @@ CREATE TABLE `users` (
   `reset_token` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
---
--- Dumping data for table `users`
---
 
 INSERT INTO `users` (`id`, `full_name`, `email`, `contact_number`, `password`, `role`, `reset_token`, `created_at`, `updated_at`) VALUES
 (16, 'vijay user', 'nawalevijay84@gmail.com', '1234567890', '$2a$10$mw6MFo.jeDWRzKciWNNFSuyiS5Ee9c4Fuc3ar2lSZ5cFwXmPwsw7G', 'Admin', NULL, '2024-11-20 19:21:56', '2024-11-20 20:20:52');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
